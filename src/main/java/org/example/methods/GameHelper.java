5b0f9d851ba5d7c4035078b3db835ceb31fadd7f
mavluda <rahimqulimavluda@gmail.com>
2025-04-25 08:43:02 +0500

    SimpleDotComGame

---
+++ src/main/java/org/example/methods/GameHelper.java
@@ -0,0 +1,25 @@
+package main.java.org.example.methods;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GameHelper {
+
+  public String getUserInput(String promt) {
+    String inputLine = null;
+    System.out.println(promt + " ");
+
+    try {
+      BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+      inputLine = is.readLine();
+      if (inputLine.length() == 0) {
+        return null;
+      }
+
+    } catch (Exception e) {
+      System.out.println("IOException: " + e);
+    }
+    return inputLine;
+  }
+
+}
---
+++ src/main/java/org/example/methods/SimpleDotComGame.java
@@ -0,0 +1,30 @@
+package main.java.org.example.methods;
+
+public class SimpleDotComGame {
+
+  public static void main(String[] args) {
+    int numOfGuesses = 0;
+
+    GameHelper gameHelper = new GameHelper();
+
+    SimpleDotCom theDotCom = new SimpleDotCom();
+
+    int randomNum = (int) (Math.random() * 5);
+    int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+    theDotCom.setLocationCells(locations);
+    boolean isAlive = true;
+
+    while (isAlive) {
+      String guess = gameHelper.getUserInput("Enter number: ");
+
+      String result = theDotCom.checkYourself(guess);
+
+      numOfGuesses++;
+
+      if (result.equals("hit")) {
+        isAlive = false;
+        System.out.println("It took you " + numOfGuesses + " guesses");
+      }
+    }
+  }
+}
